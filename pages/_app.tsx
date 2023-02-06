ffec93a3743f0f920902072ccf2724143ccfa0c2
Axel Zumwalt <axelzum@gmail.com>
2023-02-05 19:19:37 -0600

    Move navbar out of index file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
-import '../styles/globals.css';
 import {Analytics} from '@vercel/analytics/react';
-import type {AppProps} from 'next/app';
+import Head from 'next/head';
 import Script from 'next/script';
+import type {AppProps} from 'next/app';
 
+import {NavigationLayout} from '../components/navigation-layout';
+
+import '../styles/globals.css';
 
 export default function App({Component, pageProps}: AppProps): React.ReactElement {
     return (
@@ -21,38 +24,45 @@ export default function App({Component, pageProps}: AppProps): React.ReactElemen
                 strategy='afterInteractive'
             />
             <Analytics/>
+            <Head>
+                <meta charSet='utf-8' />
+                <meta content='width=device-width, initial-scale=1' name='viewport'/>
+                <meta content='true' name='HandheldFriendly'/>
 
-            <meta content='text/html; charset=utf-8' http-equiv='Content-Type'/>
-            <meta content='width=device-width, initial-scale=1' name='viewport'/>
-            <meta content='true' name='HandheldFriendly'/>
+                <meta content='Tom & Tara' name='title' />
+                <meta content='Wedding of Tom Lawrence and Tara Zumwalt' name='description' />
+                <meta content='Axel Zumwalt' name='author' />
 
-            {/* Favicons */}
-            <link href='/favicons/apple-touch-icon.png' rel='apple-touch-icon' sizes='120x120'/>
-            <link href='/favicons/favicon-32x32.png' rel='icon' sizes='32x32' type='image/png'/>
-            <link href='/favicons/favicon-16x16.png' rel='icon' sizes='16x16' type='image/png'/>
-            <link href='/favicons/site.webmanifest' rel='manifest'/>
-            <link color='#577d6a' href='/favicons/safari-pinned-tab.svg' rel='mask-icon'/>
-            <link href='/favicons/favicon.ico' rel='shortcut icon'/>
-            <meta content='#577d6a' name='msapplication-TileColor'/>
-            <meta content='/favicons/browserconfig.xml' name='msapplication-config'/>
-            <meta content='#577d6a' name='theme-color'/>
+                {/* Favicons */}
+                <link href='/favicons/apple-touch-icon.png' rel='apple-touch-icon' sizes='120x120'/>
+                <link href='/favicons/favicon-32x32.png' rel='icon' sizes='32x32' type='image/png'/>
+                <link href='/favicons/favicon-16x16.png' rel='icon' sizes='16x16' type='image/png'/>
+                <link href='/favicons/site.webmanifest' rel='manifest'/>
+                <link color='#577d6a' href='/favicons/safari-pinned-tab.svg' rel='mask-icon'/>
+                <link href='/favicons/favicon.ico' rel='shortcut icon'/>
+                <meta content='#577d6a' name='msapplication-TileColor'/>
+                <meta content='/favicons/browserconfig.xml' name='msapplication-config'/>
+                <meta content='#577d6a' name='theme-color'/>
 
-            {/* Open Graph / Facebook */}
-            <meta content='website' property='og:type'/>
-            <meta content='https://metatags.io/' property='og:url'/>
-            <meta content='Tom & Tara' property='og:title'/>
-            <meta content='Wedding of Tom Lawrence and Tara Zumwalt' property='og:description'/>
-            <meta content='/T&T-black.png' property='og:image'/>
+                {/* Open Graph / Facebook */}
+                <meta content='website' property='og:type'/>
+                <meta content='https://metatags.io/' property='og:url'/>
+                <meta content='Tom & Tara' property='og:title'/>
+                <meta content='Wedding of Tom Lawrence and Tara Zumwalt' property='og:description'/>
+                <meta content='/T&T-black.png' property='og:image'/>
 
-            {/* Twitter */}
-            <meta content='summary_large_image' property='twitter:card'/>
-            <meta content='https://metatags.io/' property='twitter:url'/>
-            <meta content='Tom & Tara' property='twitter:title'/>
-            <meta content='Wedding of Tom Lawrence and Tara Zumwalt' property='twitter:description'/>
-            <meta content='/T&T-black.png' property='twitter:image'/>
+                {/* Twitter */}
+                <meta content='summary_large_image' property='twitter:card'/>
+                <meta content='https://metatags.io/' property='twitter:url'/>
+                <meta content='Tom & Tara' property='twitter:title'/>
+                <meta content='Wedding of Tom Lawrence and Tara Zumwalt' property='twitter:description'/>
+                <meta content='/T&T-black.png' property='twitter:image'/>
 
-            <link href='https://use.typekit.net/tlo2hmw.css' rel='stylesheet'/>
-            <Component {...pageProps} />
+                <link href='https://use.typekit.net/tlo2hmw.css' rel='stylesheet'/>
+            </Head>
+            <NavigationLayout>
+                <Component {...pageProps} />
+            </NavigationLayout>
         </>
     );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,112 +1,37 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import Link from 'next/link';
-import {NavigationList} from '../components/index/navigation-list';
-import monogram from '../public/index/T&T-white.png';
+
 import tomTaraPhoto from '../public/index/tom-tara-crop-hands.jpg';
-import {useState} from 'react';
 
-/* eslint-disable @typescript-eslint/restrict-template-expressions */
+const Home = (): React.ReactElement => (
+    <>
+        <Head>
+            <title>{'Tom & Tara'}</title>
+            <meta content='Wedding of Tom Lawrence and Tara Zumwalt' name='description'/>
+        </Head>
 
-const Home = (): React.ReactElement => {
-    const [open, setOpen] = useState<boolean>(false);
-
-    return (
-        <div className='flex h-screen flex-col md:flex-row'>
-            <Head>
-                <title>{'Tom & Tara'}</title>
-                <meta content='Tom and Tara Wedding' name='description'/>
-            </Head>
-
-            {/* Top Nav */}
-            <nav className='md:hidden'>
-                <div
-                    className={`flex flex-col w-full bg-marble bg-cover transition-height duration-500 ease-in-out ${open ? 'h-36' : 'h-20'}`}
-                >
-                    <div className='flex justify-center'>
-                        <Link className='flex items-center h-20' href='/'>
-                            <Image
-                                alt='T&T Monogram Logo'
-                                className='object-contain h-[90%]'
-                                src={monogram}
-                            />
-                        </Link>
-
-                        <div className='fixed top-5 right-5 mx-auto'>
-                            <button
-                                className='flex justify-center items-center w-10 h-10'
-                                onClick={(): void => setOpen(!open)}
-                            >
-                                <span
-                                    className={`block absolute h-0.5 w-6 bg-white transform transition duration-500 ease-in-out ${open ? 'rotate-45' : '-translate-y-1.5'}`}
-                                />
-                                <span
-                                    className={`block absolute h-0.5 w-6 bg-white transform transition duration-500 ease-in-out ${open && 'opacity-0'}`}
-                                />
-                                <span
-                                    className={`block absolute h-0.5 w-6 bg-white transform transition duration-500 ease-in-out ${open ? '-rotate-45' : 'translate-y-1.5'}`}
-                                />
-                            </button>
-                        </div>
-                    </div>
-
-                    <div
-                        className={`flex flex-col h-full items-center ${open ? 'transition delay-200 duration-100 ease-in-out opacity-100' : 'opacity-0'}`}
-                    >
-                        <div className='flex flex-col items-center my-2 w-full'>
-                            <hr className='my-1 w-1/2'/>
-                            <hr className='w-1/2'/>
-                        </div>
-                        <NavigationList/>
-                    </div>
-                </div>
-            </nav>
-
-            {/* Side Nav */}
-            <nav
-                className='flex flex-col items-center hidden h-full w-1/3 bg-marble bg-cover md:flex lg:w-1/4 xl:w-1/5'
-            >
-                <Link className='mt-20' href='/'>
+        <div className='flex flex-col h-full items-center md:w-full overflow-auto'>
+            <h1 className='font-henriette my-5 lg:my-10 text-tt-green text-4xl md:text-5xl xl:text-6xl'>{'TOM & TARA'}</h1>
+            <div className='flex flex-col lg:flex-row items-center border-tt-gold border-2 w-5/6'>
+                <div className='lg:w-7/12 m-5'>
                     <Image
-                        alt='T&T Monogram Logo'
-                        src={monogram}
+                        alt='Picture of Tom and Tara'
+                        className='object-contain'
+                        priority
+                        src={tomTaraPhoto}
                     />
-                </Link>
-
-                <div className='flex flex-col items-center my-5 w-full'>
-                    <hr className='my-1 w-5/6'/>
-                    <hr className='w-5/6'/>
                 </div>
-
-                <div>
-                    <NavigationList/>
-                </div>
-            </nav>
-
-            {/* Body */}
-            <div className='flex flex-col h-full items-center md:w-full overflow-auto'>
-                <h1 className='font-henriette my-5 lg:my-10 text-tt-green text-4xl md:text-5xl xl:text-6xl'>{'TOM & TARA'}</h1>
-                <div className='flex flex-col lg:flex-row items-center border-tt-gold border-2 w-5/6'>
-                    <div className='lg:w-7/12 m-5'>
-                        <Image
-                            alt='Picture of Tom and Tara'
-                            className='object-contain'
-                            priority
-                            src={tomTaraPhoto}
-                        />
-                    </div>
-                    <div className='text-center text-tt-gold lg:w-5/12 m-5'>
-                        <p className='text-sm sm:text-lg lg:text-xl'>{'We look forward to you joining us for our wedding'}</p>
-                        <p className='text-md sm:text-xl lg:text-2xl font-henriette font-bold'>{'June 24, 2023'}</p>
-                        <p className='text-sm sm:text-lg lg:text-lg'>{'in Cedar Rapids, Iowa'}</p>
-                    </div>
-                </div>
-                <div className='w-5/6 mt-5 text-center text-md sm:text-lg lg:text-xl text-tt-gold italic'>
-                    {'Check back, additional details will be added soon'}
+                <div className='text-center text-tt-gold lg:w-5/12 m-5'>
+                    <p className='text-sm sm:text-lg lg:text-xl'>{'We look forward to you joining us for our wedding'}</p>
+                    <p className='text-md sm:text-xl lg:text-2xl font-henriette font-bold'>{'June 24, 2023'}</p>
+                    <p className='text-sm sm:text-lg lg:text-lg'>{'in Cedar Rapids, Iowa'}</p>
                 </div>
             </div>
+            <div className='w-5/6 mt-5 text-center text-md sm:text-lg lg:text-xl text-tt-gold italic'>
+                {'Check back, additional details will be added soon'}
+            </div>
         </div>
-    );
-};
+    </>
+);
 
 export default Home;
