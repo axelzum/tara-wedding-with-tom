48285cdb13b50e4eaeaff613c2a79e2e9c59f1e8
Axel Zumwalt <axelzum@gmail.com>
2023-02-11 17:00:50 -0600

    Fix map iframe

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,14 +38,13 @@ const Home = (): React.ReactElement => (
 
             <div className='flex flex-col my-5 items-center w-1/2 pb-6'>
                 <p className='font-fino text-center text-2xl md:text-3xl xl:text-4xl'>{'Ceremony & Reception'}</p>
-                <p className='font-quiche text-center text-lg'>{'HarMac Event Center, Downtown Cedar Rapids'}</p>
-                <p className='font-quiche text-center text-lg'>{'Late Afternoon – Schedule Pending'}</p>
+                <p className='font-quiche text-center text-sm md:text-md xl:text-lg'>{'HarMac Event Center, Downtown Cedar Rapids'}</p>
+                <p className='font-quiche text-center text-sm md:text-md xl:text-lg'>{'Late Afternoon – Schedule Pending'}</p>
                 <iframe
-                    height='300'
+                    className='w-full h-[250px] md:h-[300px] lg:h-[400px]'
                     loading='lazy'
                     referrerPolicy='no-referrer-when-downgrade'
                     src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3143.80122729655!2d-91.66361284886887!3d41.97576737911249!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87e4f75658da978f%3A0x611e858f80808957!2sThe%20Harmac!5e1!3m2!1sen!2sus!4v1676152152275!5m2!1sen!2sus'
-                    width='400'
                 />
             </div>
         </div>
