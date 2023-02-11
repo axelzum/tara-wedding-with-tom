ba2a252a2f21b87e948048d575c0fa33cbe35bb8
Axel Zumwalt <axelzum@gmail.com>
2023-02-11 16:50:11 -0600

    Update favicons/meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,40 +25,39 @@ export default function App({Component, pageProps}: AppProps): React.ReactElemen
             />
             <Analytics/>
             <Head>
-                <meta charSet='utf-8' />
+                <meta charSet='utf-8'/>
                 <meta content='width=device-width, initial-scale=1' name='viewport'/>
                 <meta content='true' name='HandheldFriendly'/>
 
-                <meta content='Tom & Tara' name='title' />
-                <meta content='Wedding of Tom Lawrence and Tara Zumwalt' name='description' />
-                <meta content='Axel Zumwalt' name='author' />
+                <title>{'Tom & Tara'}</title>
+                <meta content='Tom & Tara' name='title'/>
+                <meta content='Wedding of Tom Lawrence and Tara Zumwalt' name='description'/>
+                <meta content='Axel Zumwalt' name='author'/>
 
                 {/* Favicons */}
-                <link href='/favicons/apple-touch-icon.png' rel='apple-touch-icon' sizes='120x120'/>
+                <link href='/favicons/apple-touch-icon.png' rel='apple-touch-icon' sizes='180x180'/>
                 <link href='/favicons/favicon-32x32.png' rel='icon' sizes='32x32' type='image/png'/>
                 <link href='/favicons/favicon-16x16.png' rel='icon' sizes='16x16' type='image/png'/>
                 <link href='/favicons/site.webmanifest' rel='manifest'/>
-                <link color='#577d6a' href='/favicons/safari-pinned-tab.svg' rel='mask-icon'/>
+                <link color='#000000' href='/favicons/safari-pinned-tab.svg' rel='mask-icon'/>
                 <link href='/favicons/favicon.ico' rel='shortcut icon'/>
-                <meta content='#577d6a' name='msapplication-TileColor'/>
+                <meta content='#ffffff' name='msapplication-TileColor'/>
                 <meta content='/favicons/browserconfig.xml' name='msapplication-config'/>
-                <meta content='#577d6a' name='theme-color'/>
+                <meta content='#ffffff' name='theme-color'/>
 
                 {/* Open Graph / Facebook */}
                 <meta content='website' property='og:type'/>
-                <meta content='https://metatags.io/' property='og:url'/>
+                <meta content='https://tomtarawedding.com/' property='og:url'/>
                 <meta content='Tom & Tara' property='og:title'/>
                 <meta content='Wedding of Tom Lawrence and Tara Zumwalt' property='og:description'/>
-                <meta content='/T&T-black.png' property='og:image'/>
+                <meta content='/images/home/tom-tara-crop-hands.jpg' property='og:image'/>
 
                 {/* Twitter */}
                 <meta content='summary_large_image' property='twitter:card'/>
-                <meta content='https://metatags.io/' property='twitter:url'/>
+                <meta content='https://tomtarawedding.com/' property='twitter:url'/>
                 <meta content='Tom & Tara' property='twitter:title'/>
                 <meta content='Wedding of Tom Lawrence and Tara Zumwalt' property='twitter:description'/>
-                <meta content='/T&T-black.png' property='twitter:image'/>
-
-                <link href='https://use.typekit.net/tlo2hmw.css' rel='stylesheet'/>
+                <meta content='/images/home/tom-tara-crop-hands.jpg' property='twitter:image'/>
             </Head>
             <NavigationLayout>
                 <Component {...pageProps} />
